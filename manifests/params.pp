9907dedffddce1cf14304b557781fa20cf03e4dc
Israel Calvete <icalvete@gmail.com>
2016-06-24 11:29:41 +0200

    Decoupling from hiera-

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,27 +3,28 @@ class kibana4::params {
   $webserver_user            = 'www-data'
   $webserver_group           = 'www-data'
 
-  $org_domain                = hiera('org_domain')
+  $org_domain                = pick(hiera('org_domain'), 'example.net')
 
-  $repo_scheme               = hiera('sp_repo_scheme')
-  $repo_domain               = hiera('sp_repo_domain')
-  $repo_port                 = hiera('sp_repo_port')
-  $repo_user                 = hiera('sp_repo_user')
-  $repo_pass                 = hiera('sp_repo_pass')
-  $repo_path                 = hiera('sp_repo_path')
-  $package                   = hiera('kibana_package')
+  $repo_scheme               = pick(hiera('sp_repo_scheme'), 'http')
+  $repo_domain               = pick(hiera('sp_repo_domain'), 'smarpurposes-repo.s3-website-eu-west-1.amazonaws.com')
+  $repo_port                 = pick_default(hiera('sp_repo_port'), '')
+  $repo_user                 = pick_default(hiera('sp_repo_user'), '')
+  $repo_pass                 = pick_default(hiera('sp_repo_pass'), '')
+  $repo_path                 = pick_default(hiera('sp_repo_path'), '')
+  $package                   = pick(hiera('kibana_package'), 'kibana-4.5.1-linux-x64.tar.gz')
 
   $elasticsearch_server      = 'localhost:9200'
-  $kibana_document_root      = hiera('kibana_document_root')
-  $kibana_htpasswd_file      = hiera('htpasswd_file')
-  $kibana_htpasswd_user      = hiera('kibana_user')
-  $kibana_htpasswd_pass      = hiera('kibana_pass')
+  $kibana_document_root      = pick(hiera('kibana_document_root'), '/srv/kibana')
+  $kibana_htpasswd_file      = pick(hiera('htpasswd_file'), '/etc/apache2/htpasswd')
+  $kibana_htpasswd_user      = pick(hiera('kibana_user'), 'kibana')
+  $kibana_htpasswd_pass      = pick(hiera('kibana_pass'), 'kibana')
 
   $bin_dir                   = 'bin'
+  $bin_file                  = "${bin_dir}/kibana"
   $config_dir                = 'config'
   $config_file               = "${config_dir}/kibana.yml"
 
   $elasticsearch_server_auth = false
-  $elasticsearch_user        = hiera('kibana_user')
-  $elasticsearch_user_pass   = hiera('kibana_pass')
+  $elasticsearch_user        = pick(hiera('kibana_user'), 'kibana')
+  $elasticsearch_user_pass   = pick(hiera('kibana_user'), 'kibana')
 }
